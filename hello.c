c7a0be7dd04bd9e6031cdc3ac7c86677d8a56506
btlaube <blaube2@huskers.unl.edu>
2020-09-02 23:38:38 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author Ben Laube
+ * Date: 2020/09/01
+ *
+ * Hello world program
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Ben Laube\n");
+
+     return 0;
+ }
